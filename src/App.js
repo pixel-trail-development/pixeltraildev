7d28c531b0a014fbd5c3a3d2aaba6604712a8966
Ethan Luxton <luxtonethan@hotmail.com>
2025-05-17 15:43:54 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Home from './Components/Body/Home/Home.jsx';
-import Pricing from './Components/Body/Pricing/Pricing.jsx';
-import About from './Components/Body/About/About.jsx';
+
 import Footer from './Components/Footer/Footer.jsx'
 import ContactPricing from './Components/Body/Contact/ContactPricing.jsx';
 import { SubConfirm } from './Components/Body/Contact/SubConfirm.jsx';
@@ -20,8 +19,8 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/pricing" element={<Pricing />} />
-                <Route path="/about" element={<About />} />
+             
+                
                 <Route path="/contact" element={<ContactPricing />} />        
                 <Route path="/contact-confirmed" element={<SubConfirm />} />
                 <Route path="*" element={<NotFound/>} />
--- src/Components/Body/Home/Home.jsx
+++ src/Components/Body/Home/Home.jsx
@@ -3,25 +3,16 @@ import {
   Typography,
   Container,
   Grid,
-  Card,
-  CardContent,
   Button,
 } from '@mui/material';
 import Image from '../../../Assets/PixelTrail.png';
-import { SocialIcon } from 'react-social-icons';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import data from '../../data.json';
 import { Typewriter } from 'react-simple-typewriter';
 import { Link, useLocation } from 'react-router-dom';
 import SEO from '../../SEO.jsx';
 import ReactGAComponent from '../../../ReactGA.jsx';
 
-const social = [
-  { url: "https://www.x.com/pixeltraildev", label: "Twitter" },
-  { url: "https://www.instagram.com/pixeltraildev/?hl=en", label: "Instagram" },
-  { url: "https://www.facebook.com/profile.php?id=61560728469609", label: "Facebook" },
-  { url: "https://www.linkedin.com/company/pixel-trail-development-llc/", label: "LinkedIn" },
-];
+
 
 function Home() {
   const location = useLocation();
@@ -100,44 +91,8 @@ function Home() {
               </Grid>
             </Grid>
           </section>
-          <Typography variant="h1" className="headline templates-section">
-            {data.servicesSection.headlineTwo}
-          </Typography>
-          <Grid container spacing={4} className="template-examples">
-            {data.servicesSection.templates.map((template, index) => (
-              <Grid item xs={12} sm={6} md={3} key={index}>
-                <Card className="template-card">
-                  <CardContent>
-                    <div className="category-container">
-                      <Typography variant="overline" className="category">
-                        {template.category}
-                      </Typography>
-                      <ArrowForwardIcon className="arrow-icon" aria-hidden="true" />
-                    </div>
-                    <Typography variant="h6" className="template-name">
-                      {template.name}
-                    </Typography>
-                    <Typography variant="body2" className="template-description">
-                      {template.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-          <div className='social-container'>
-            <Typography variant="h1" className='headline'>Let's connect</Typography>
-            <div className='social-icons'>
-              {social.map((socialItem, index) => (
-                <SocialIcon 
-                  key={index} 
-                  url={socialItem.url} 
-                  aria-label={socialItem.label} 
-                  className="social-icon" 
-                />
-              ))}
-            </div>
-          </div>
+          
+          
         </Container>
       </div>
     </>
